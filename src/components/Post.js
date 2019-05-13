771cb10f32238614ae728391f5c4681e801d170d
Alamin Mir <alaminip@gmail.com>
2019-05-13 16:20:10 +0600

    redux

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // import axios from 'axios';
 import { connect } from 'react-redux';
+import { deletePost } from '../actions/postActions';
 
 class Post extends Component {
     // state = {
@@ -18,12 +19,17 @@ class Post extends Component {
     //     })
     // }
 
+    handleClick = () => {
+        this.props.deletePost(this.props.post.id);
+    }
+
     render() {
         console.log(this.props)
         const post = this.props.post ? (
             <div className="container">
                 <h4>{this.props.post.title}</h4>
                 <p>{this.props.post.body}</p>
+                <button className="btn btn-danger btn-sm" onClick={this.handleClick}>Delete Post</button>
             </div>
         ) : (
             <div>Loading...</div>
@@ -38,9 +44,17 @@ class Post extends Component {
 
 const mapStateToProps = (state, ownProps) => {
     let id = ownProps.match.params.post_id;
+    // console.log(id);
     return {
         post: state.posts.find(post => post.id = id)
+        // post: state.posts.post.id = '3'
+    }    
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: (id) => { dispatch(deletePost(id)) }
     }
 }
 
-export default connect(mapStateToProps)(Post);
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
